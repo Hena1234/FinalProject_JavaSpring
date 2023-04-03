563b4e5f7407f840d15511c0654b49a76f800329
Beatriz <93779842+biaanjos@users.noreply.github.com>
2023-04-03 17:55:02 -0400

    Create DistributionCentreRepository.java

---
+++ src/main/java/cpan228/finalProject/repository/DistributionCentreRepository.java
@@ -0,0 +1,7 @@
+package com.cpan228.finalProject.repository;
+
+import com.cpan228.finalProject.model.DistributionCentre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DistributionCentreRepository extends JpaRepository<DistributionCentre, Long> {
+}
